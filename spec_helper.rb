aa63d19167cac38dd235476b5a7311a03b30a847
IFctor <IFctor@DESKTOP-0RVIM66>
2018-04-15 18:05:42 +0200

    spec add

---
+++ spec_helper.rb
@@ -0,0 +1,4 @@
+require 'coveralls'
+Coveralls.wear!
+
+https://coveralls.io/repos/github/IFctor/MinBizBead/badge.svg?branch=master(Coverage Status)!:https://coveralls.io/github/IFctor/MinBizBead?branch=master
